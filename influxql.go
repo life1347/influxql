2a5ae82c0412dda178fd61360a5435054416d174
José Carlos Nieto <jose.carlos@menteslibres.net>
2016-03-16 12:00:06 -0600

    wip: Adding more tests and testing different cases.

--- influxql.go
+++ influxql.go
@@ -2,7 +2,6 @@ package influxql
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"regexp"
 	"strings"
@@ -10,35 +9,191 @@ import (
 	"time"
 )
 
-var reWhiteChars = regexp.MustCompile(`[\s\t\r\n]+`)
+const placeholder = "?"
+
+type nullValue struct{}
+
+var (
+	reWhiteChars        = regexp.MustCompile(`[\s\t\r\n]+`)
+	reSpacesBetweenTags = regexp.MustCompile(`}}[\s\t\r\n]+{{`)
+)
 
 func cleanTemplate(s string) string {
-	return strings.TrimSpace(reWhiteChars.ReplaceAllString(s, " "))
+	s = reWhiteChars.ReplaceAllString(s, " ")
+	s = reSpacesBetweenTags.ReplaceAllString(s, "}}{{")
+	return strings.TrimSpace(s)
 }
+
+type quoted string
 
 const selectTemplateText = `
 	SELECT
 		{{.Fields | joinWithCommas}}
 	FROM
 		{{.Measurement}}
+	{{if .Where}}
+		WHERE
+		 {{.Where | joinWithSpace }}
+	{{end}}
+	{{if .GroupBy}}
+		GROUP BY
+		 {{.GroupBy | joinWithCommas }}
+	{{end}}
+	{{if .Fill}} fill({{.Fill}}){{end}}
 `
 
 type selectTemplateValues struct {
 	Measurement string
 	Fields      []string
+	Where       []string
+	GroupBy     []string
+	Fill        string
 }
 
 func joinWithCommas(in []string) string {
 	return strings.Join(in, ", ")
 }
 
+func joinWithSpace(in []string) string {
+	return strings.Join(in, " ")
+}
+
 var selectTemplate = template.Must(
 	template.New("select").Funcs(
 		map[string]interface{}{
 			"joinWithCommas": joinWithCommas,
+			"joinWithSpace":  joinWithSpace,
 		},
 	).Parse(cleanTemplate(selectTemplateText)),
 )
+
+type timeGroup struct {
+	d time.Duration
+}
+
+func (t *timeGroup) Compile() (string, error) {
+	ns := t.d.Nanoseconds()
+
+	switch int64(0) {
+	case ns % int64(time.Hour):
+		return fmt.Sprintf("time(%dh)", ns/int64(time.Hour)), nil
+	case ns % int64(time.Minute):
+		return fmt.Sprintf("time(%dm)", ns/int64(time.Minute)), nil
+	case ns % int64(time.Second):
+		return fmt.Sprintf("time(%ds)", ns/int64(time.Second)), nil
+	}
+
+	return fmt.Sprintf("time(%dns)", ns), nil
+}
+
+func Time(duration time.Duration) compilable {
+	return &timeGroup{d: duration}
+}
+
+type customFunc struct {
+	name  string
+	alias string
+	args  []interface{}
+}
+
+func (f *customFunc) As(alias string) *customFunc {
+	f.alias = alias
+	return f
+}
+
+func (f *customFunc) Compile() (string, error) {
+	if f.name == "" {
+		return "", fmt.Errorf("Missing function name.")
+	}
+	args := make([]string, 0, len(f.args))
+	for _, arg := range f.args {
+		var s string
+		switch t := arg.(type) {
+		case compilable:
+			var err error
+			s, err = t.Compile()
+			if err != nil {
+				return "", err
+			}
+		default:
+			s = fmt.Sprintf("%v", t)
+		}
+		args = append(args, s)
+	}
+	fn := fmt.Sprintf("%s(%s)", f.name, strings.Join(args, ", "))
+	if f.alias != "" {
+		fn = fmt.Sprintf("%s AS %q", fn, f.alias)
+	}
+	return fn, nil
+}
+
+func Func(name string, args ...interface{}) *customFunc {
+	return &customFunc{name: name, args: args}
+}
+
+func Count(field interface{}) *customFunc {
+	return Func("COUNT", []interface{}{&literal{field}}...)
+}
+
+func Mean(field string) *customFunc {
+	return Func("MEAN", []interface{}{&literal{field}}...)
+}
+
+func Median(field string) *customFunc {
+	return Func("MEDIAN", []interface{}{&literal{field}}...)
+}
+
+func Spread(field string) *customFunc {
+	return Func("SPREAD", []interface{}{&literal{field}}...)
+}
+
+func Sum(field string) *customFunc {
+	return Func("SUM", []interface{}{&literal{field}}...)
+}
+
+func Bottom(field string, params ...interface{}) *customFunc {
+	return Func("SUM", append([]interface{}{&literal{field}}, params...))
+}
+
+func Top(field string, params ...interface{}) *customFunc {
+	return Func("TOP", append([]interface{}{&literal{field}}, params...))
+}
+
+func Derivative(field string, params ...interface{}) *customFunc {
+	return Func("DERIVATIVE", append([]interface{}{&literal{field}}, params...))
+}
+
+func NonNegativeDerivative(field string, params ...interface{}) *customFunc {
+	return Func("NON_NEGATIVE_DERIVATIVE", append([]interface{}{&literal{field}}, params...))
+}
+
+func First(field string) *customFunc {
+	return Func("FIRST", []interface{}{&literal{field}}...)
+}
+
+func StdDev(field string) *customFunc {
+	return Func("STDDEV", []interface{}{&literal{field}}...)
+}
+
+func Last(field string) *customFunc {
+	return Func("LAST", []interface{}{&literal{field}}...)
+}
+
+func Max(field string) *customFunc {
+	return Func("MAX", []interface{}{&literal{field}}...)
+}
+
+func Min(field string) *customFunc {
+	return Func("MIN", []interface{}{&literal{field}}...)
+}
+
+func Distinct(field string) *customFunc {
+	return Func("DISTINCT", []interface{}{&literal{field}}...)
+}
+
+func Percentile(field string, p float64) *customFunc {
+	return Func("PERCENTILE", []interface{}{&literal{field}, p})
+}
 
 type Builder interface {
 	Build() (string, error)
@@ -67,13 +222,41 @@ type Expr struct {
 }
 
 func (e *Expr) Compile() (string, error) {
-	if strings.Count("?", e.expr) != len(e.values) {
-		return "", errors.New("Mismatched number of placeholders and values")
+	placeholders := strings.Count(e.expr, placeholder)
+
+	if placeholders > 0 {
+		// Where("foo = ?", "bar")
+		if placeholders != len(e.values) {
+			return "", fmt.Errorf("Mismatched number of placeholders (%d) and values (%d)", strings.Count(e.expr, placeholder), len(e.values))
+		}
+	} else {
+		if len(e.values) > 0 {
+			parts := strings.Split(strings.TrimSpace(reWhiteChars.ReplaceAllString(e.expr, " ")), " ")
+			lparts := len(parts)
+
+			if lparts < 1 {
+				return "", fmt.Errorf("Expecting statement.")
+			} else if lparts < 2 {
+				// Where("foo", "bar")
+				if len(e.values) != 1 {
+					return "", fmt.Errorf("Expecting exactly one value.")
+				}
+				e.expr = fmt.Sprintf("%q = ?", parts[0])
+			} else if lparts < 3 {
+				// Where("foo =", "bar")
+				if len(e.values) != 1 {
+					return "", fmt.Errorf("Expecting exactly one value.")
+				}
+				e.expr = fmt.Sprintf("%q %s ?", parts[0], parts[1])
+			} else {
+				return "", fmt.Errorf("Unsupported expression %q", e.expr)
+			}
+		}
 	}
 
 	compiled := make([]interface{}, 0, len(e.values))
 	for i := range e.values {
-		lit := &literal{e.values[i]}
+		lit := &value{e.values[i]}
 		c, err := lit.Compile()
 		if err != nil {
 			return "", err
@@ -85,36 +268,50 @@ func (e *Expr) Compile() (string, error) {
 	return fmt.Sprintf(s, compiled...), nil
 }
 
+type value struct {
+	v interface{}
+}
+
+func (v *value) Compile() (string, error) {
+	switch t := v.v.(type) {
+	case string:
+		return fmt.Sprintf(`'%s'`, t), nil
+	case int:
+		return fmt.Sprintf("%d", t), nil
+	case uint:
+		return fmt.Sprintf("%d", t), nil
+	case int64:
+		return fmt.Sprintf("%d", t), nil
+	case uint64:
+		return fmt.Sprintf("%d", t), nil
+	case int32:
+		return fmt.Sprintf("%d", t), nil
+	case uint32:
+		return fmt.Sprintf("%d", t), nil
+	case int8:
+		return fmt.Sprintf("%d", t), nil
+	case uint8:
+		return fmt.Sprintf("%d", t), nil
+	case time.Duration:
+		return fmt.Sprintf("%dns", t.Nanoseconds()), nil
+	default:
+		return fmt.Sprintf(`'%v'`, t), nil
+	}
+	panic("reached")
+}
+
 type literal struct {
 	v interface{}
 }
 
 func (l *literal) Compile() (string, error) {
 	switch v := l.v.(type) {
-	case string:
-		return fmt.Sprintf("%q", v), nil
-	case int:
-		return fmt.Sprintf("%d", v), nil
-	case uint:
-		return fmt.Sprintf("%d", v), nil
-	case int64:
-		return fmt.Sprintf("%d", v), nil
-	case uint64:
-		return fmt.Sprintf("%d", v), nil
-	case int32:
-		return fmt.Sprintf("%d", v), nil
-	case uint32:
-		return fmt.Sprintf("%d", v), nil
-	case int8:
-		return fmt.Sprintf("%d", v), nil
-	case uint8:
-		return fmt.Sprintf("%d", v), nil
-	case time.Duration:
-		return fmt.Sprintf("%dns", v.Nanoseconds()), nil
 	case compilable:
 		return v.Compile()
+	case string:
+		return fmt.Sprintf(`"%s"`, v), nil
 	default:
-		return fmt.Sprintf("%v", v), nil
+		return fmt.Sprintf(`"%v"`, v), nil
 	}
 	panic("reached")
 }
@@ -129,6 +326,7 @@ type selectBuilder struct {
 	offset      int
 	slimit      int
 	soffset     int
+	fill        interface{}
 }
 
 func Select(fields ...interface{}) *selectBuilder {
@@ -136,6 +334,15 @@ func Select(fields ...interface{}) *selectBuilder {
 	for i := range fields {
 		s.fields = append(s.fields, &literal{fields[i]})
 	}
+	return s
+}
+
+func (s *selectBuilder) Fill(v interface{}) *selectBuilder {
+	if v == nil {
+		s.fill = nullValue{}
+		return s
+	}
+	s.fill = v
 	return s
 }
 
@@ -203,6 +410,25 @@ func (s *selectBuilder) Build() (string, error) {
 
 	if err := compileArrayInto(s.fields, &data.Fields); err != nil {
 		return "", err
+	}
+
+	if err := compileArrayInto(s.where, &data.Where); err != nil {
+		return "", err
+	}
+
+	if err := compileArrayInto(s.groupBy, &data.GroupBy); err != nil {
+		return "", err
+	}
+
+	if s.fill != nil {
+		switch v := s.fill.(type) {
+		case nullValue:
+			data.Fill = "null"
+		case string:
+			data.Fill = fmt.Sprintf("%v", v)
+		default:
+			data.Fill = fmt.Sprintf("%v", v)
+		}
 	}
 
 	buf := bytes.NewBuffer(nil)
--- influxql_test.go
+++ influxql_test.go
@@ -3,13 +3,136 @@ package influxql
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
+var testSamples = []struct {
+	b Builder
+	s string
+	e bool
+}{
+	{
+		Select("foo").From("bar"),
+		`SELECT "foo" FROM "bar"`,
+		false,
+	},
+	{
+		Select("foo").From("bar").Where(`location = ?`, "Toronto"),
+		`SELECT "foo" FROM "bar" WHERE location = 'Toronto'`,
+		false,
+	},
+	{
+		Select("foo").From("bar").Where(`location != ?`, "Toronto"),
+		`SELECT "foo" FROM "bar" WHERE location != 'Toronto'`,
+		false,
+	},
+	{
+		Select("foo").From("bar").Where(`location != 'Toronto'`),
+		`SELECT "foo" FROM "bar" WHERE location != 'Toronto'`,
+		false,
+	},
+	{
+		Select("foo").From("bar").Where(`location`, "Toronto"),
+		`SELECT "foo" FROM "bar" WHERE "location" = 'Toronto'`,
+		false,
+	},
+	{
+		Select("foo").From("bar").Where(`"location" = ?`, "Toronto"),
+		`SELECT "foo" FROM "bar" WHERE "location" = 'Toronto'`,
+		false,
+	},
+	{
+		Select("foo").From("bar").Where(`"location" = ? ?`, "Toronto"),
+		``,
+		true, // Unmatched ? placeholder.
+	},
+	{
+		Select("foo").From("bar").Where(`"location" = ? AND "altitude" >= ?`, "Toronto", 500),
+		`SELECT "foo" FROM "bar" WHERE "location" = 'Toronto' AND "altitude" >= 500`,
+		false,
+	},
+	{
+		Select("foo").From("bar").Where("location", "Toronto").And("altitude >=", 500),
+		`SELECT "foo" FROM "bar" WHERE "location" = 'Toronto' AND "altitude" >= 500`,
+		false,
+	},
+	{
+		Select("foo").From("bar").Where("location", "Toronto").Or("altitude >=", 500),
+		`SELECT "foo" FROM "bar" WHERE "location" = 'Toronto' OR "altitude" >= 500`,
+		false,
+	},
+	{
+		Select("foo").From("bar").Where("location", "Toronto").And("altitude >= x", 500),
+		``,
+		true, // Unsupported expression "altitude >= x"
+	},
+	{
+		Select(Mean("value")).From("cpu").Where("region", "uswest"),
+		`SELECT MEAN("value") FROM "cpu" WHERE "region" = 'uswest'`,
+		false, // Query is invalid, though.
+	},
+	{
+		Select(Sum("value")).From("cpu").Where("region", "uswest"),
+		`SELECT SUM("value") FROM "cpu" WHERE "region" = 'uswest'`,
+		false, // Query is invalid, though.
+	},
+	{
+		Select(Distinct("level description")).From("h2o_feet"),
+		`SELECT DISTINCT("level description") FROM "h2o_feet"`,
+		false,
+	},
+	{
+		Select(Min("water_level"), Max("water_level")).From("h2o_feet"),
+		`SELECT MIN("water_level"), MAX("water_level") FROM "h2o_feet"`,
+		false,
+	},
+	{
+		Select(Mean("water_level").As("dream_name")).From("h2o_feet"),
+		`SELECT MEAN("water_level") AS "dream_name" FROM "h2o_feet"`,
+		false,
+	},
+	{
+		Select(Min("water_level").As("mwl"), Max("water_level").As("Mwl")).From("h2o_feet"),
+		`SELECT MIN("water_level") AS "mwl", MAX("water_level") AS "Mwl" FROM "h2o_feet"`,
+		false,
+	},
+	{
+		Select(Count(Distinct("level description"))).From("h2o_feet"),
+		`SELECT COUNT(DISTINCT("level description")) FROM "h2o_feet"`,
+		false,
+	},
+	{
+		Select(Mean("value")).From("cpu").Where("region", "uswest").GroupBy(Time(time.Minute * 10)),
+		`SELECT MEAN("value") FROM "cpu" WHERE "region" = 'uswest' GROUP BY time(10m)`,
+		false,
+	},
+	{
+		Select(Mean("value")).From("cpu").Where("region", "uswest").GroupBy(Time(time.Minute * 10)).Fill(0),
+		`SELECT MEAN("value") FROM "cpu" WHERE "region" = 'uswest' GROUP BY time(10m) fill(0)`,
+		false,
+	},
+	{
+		Select(Mean("value")).From("cpu").Where("region", "uswest").GroupBy(Time(time.Minute * 10)).Fill(nil),
+		`SELECT MEAN("value") FROM "cpu" WHERE "region" = 'uswest' GROUP BY time(10m) fill(null)`,
+		false,
+	},
+	{
+		Select(Mean("value")).From("cpu").Where("region", "uswest").GroupBy(Time(time.Hour * 4)).Fill("none"),
+		`SELECT MEAN("value") FROM "cpu" WHERE "region" = 'uswest' GROUP BY time(4h) fill(none)`,
+		false,
+	},
+}
+
 func TestSelect(t *testing.T) {
-	q := Select("foo").From("bar")
+	for _, sample := range testSamples {
+		s, err := sample.b.Build()
 
-	s, err := q.Build()
+		if sample.e {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+		}
 
-	assert.NoError(t, err)
-	assert.Equal(t, `SELECT "foo" FROM "bar"`, s)
+		assert.Equal(t, sample.s, s)
+	}
 }
